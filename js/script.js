077a2291d1ad38b01f051e0ce62070f603d49b21
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-12-01 09:36:53 +0200

    Adds option to save total hunters value to local storage Changes to be committed: 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+const LOCAL_STORAGE_KEY = 'total-number';
 const valueInput = document.querySelector('input[data-totalHunters]');
 const chooseBtn = document.querySelector('button[data-chooseBtn]');
-let result = document.querySelector('.js-result');
+const result = document.querySelector('.js-result');
 
-chooseBtn.addEventListener('click', function () {
+const OnPageReload = () => {
+  const savedTotalHunters = localStorage.getItem(LOCAL_STORAGE_KEY);
+  valueInput.value = savedTotalHunters ? savedTotalHunters : undefined;
+};
+
+OnPageReload();
+
+valueInput.addEventListener('input', e => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, e.target.value);
+});
+
+chooseBtn.addEventListener('click', () => {
   const hunters = Number(valueInput.value);
   result.textContent = Math.floor(1 + Math.random() * hunters);
 });
